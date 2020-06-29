d74531eca3126d0b5b15a427158f326cb5ee8e1e
Elkan Frank <elkan.frank@gmail.com>
2020-06-29 20:21:06 +0200

    now using void array

--- test.c
+++ test.c
@@ -8,14 +8,5 @@ int main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	bmp = read_bmp(fd);
 	print_meta_data(bmp.info);
-	int i;
-	i = 0;
-	while (i < bmp.info.height * bmp.info.width)
-	{
-		print_pixel(bmp.pixels[i]);
-		i++;
-		if (i % (bmp.info.width) == 0)
-			printf("\n");
-	}
 	return (0);
 }
